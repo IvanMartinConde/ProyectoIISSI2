13d416f7f8b375a2e1247b21cac2ec8b7f5e0ece
IvanMartinConde <ivamarcon1@alum.us.es>
2024-03-01 10:49:01 +0100

    NUEVO

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,6 +6,92 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+
+      },
+      description:{
+        allowNull: true,
+        type: Sequelize.TEXT
+      },
+      address:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalcode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: true, 
+        type: Sequelize.STRING
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      phone : {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      logo: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      heroImage: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      status:{
+        allowNull:false,
+        type: Sequelize.ENUM,
+        values: [
+          'online',
+          'offline',
+          'closed',
+          'temporaly closed'
+        ],
+        defaultValue:'online'
+      },
+      averageServiceMinutes:{
+        allowNull:false,
+        type:Sequelize.DOUBLE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      restaurantCategoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
       }
       // TODO: Include the rest of the fields of the Restaurants table
 
